cbe103ef25ad802ad2c765dff4667b622140d862
Jallappino <stefan.murakami@gmail.com>
2018-03-06 00:55:02 -0500

    updated first 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+
 // Make h1 in header have different text
+header = document.querySelector('h1'); 
+header.innerText= "new text!"; 
+
 // Make nav ul li elements get larger when you hover over them
+list = document.quereySelector('ul')
+list.addEventListener('mouseenter',function() {
+	list.style.fontSize = '30px';
+})
+
+list.addEventListener('mouseout',function() {
+	list.style.fontsize = '15px';
+});
 // Add another element to the main element
 // Dynamically change the color of the background on the footer when the user hovers over it
 // Change the font size of the paragraphs in the main element when the user clicks them
@@ -8,3 +20,4 @@
 // Put something in the place of the h1 in the header
 // Change the text in one of the paragraph tags in the main element
 // Change the size of the header element to 200px using grid syntax
+
