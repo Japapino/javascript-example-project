8c46f90fe92d17b6c1bd7d45ca89c68c1d366f96
Jallippino <35497750+Japapino@users.noreply.github.com>
2018-03-06 01:50:19 -0500

    finished 1-5 and 7

--- js/app.js
+++ js/app.js
@@ -14,6 +14,11 @@ list.style.fontSize = '15px';
 });
 
 // Add another element to the main element
+maintext = document.querySelector('main');
+txt3 = document.createElement("p");  // Create with DOM
+txt3.innerHTML = "Text.";
+maintext.append(txt3);      // Append the new elements
+
 // Dynamically change the color of the background on the footer when the user hovers over it
 bottom = document.querySelector('footer');
 bottom.addEventListener('mouseenter', function() {
@@ -24,8 +29,17 @@ bottom.addEventListener('mouseleave', function() {
 	bottom.style.backgroundColor = 'green'; 
 });
 // Change the font size of the paragraphs in the main element when the user clicks them
+pfont = document.querySelector('main'); 
+pfont.addEventListener('mouseenter', function(){
+	pfont.style.fontSize = '30px'; 
+})
+pfont.addEventListener('mouseleave', function(){
+	pfont.style.fontSize = '20px'; 
+})
 // Change the text in the footer when the user clicks it
 // Remove the h1 element in the header
+header.remove('h1'); 
+
 // Put something in the place of the h1 in the header
 // Change the text in one of the paragraph tags in the main element
 // Change the size of the header element to 200px using grid syntax
